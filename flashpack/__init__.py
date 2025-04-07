920b225681913e2951a19e520b5f74602773c793
Casper Hansen <casperbh.96@gmail.com>
2025-04-07 08:57:18 +0000

    import all chat templates

--- flashpack/__init__.py
+++ flashpack/__init__.py
@@ -1,6 +1,6 @@
 from flashpack.patch import patch_for_multipack
 from flashpack.attention_utils import get_unpad_data
-from flashpack.chat_templates import qwen25_template
+from flashpack.chat_templates import *
 from flashpack.collator import V2BatchSamplerDataCollatorForSeq2Seq
 from flashpack.sampler import MultipackBatchSampler
 from flashpack.dataset import (
